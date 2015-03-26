6236f27e82536f8b593194221f556cf3477cb55b
Bastian Aigner <Basti@Bastians-MacBook-Pro.local>
2015-03-26 12:00:50 +0100

    Initial Commit

---
+++ WWDC15/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WWDC15
+//
+//  Created by Bastian Aigner on 26.03.15.
+//  Copyright (c) 2015 Bastian Aigner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
